8172d5bea4f51b0a91b815340cdd5d8c1eefba4d
waterlulu12 <aatishshrestha123@gmail.com>
2024-09-06 02:41:06 +0545

    feat: fakeAPI for demo

---
+++ src/utils/responseAPI.ts
@@ -0,0 +1,15 @@
+import fakeData from "./responseData";
+import { responseType } from "./types/responseType";
+
+export const fetchFakeData = (phoneNumber: string): Promise<responseType> => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const result = fakeData.find((data: responseType) => data.phoneNumber === phoneNumber);
+      if (result) {
+        resolve(result);
+      } else {
+        reject(new Error("No data found for the provided phone number."));
+      }
+    }, 5000);
+  });
+};
---
+++ src/utils/types/responseType.ts
@@ -0,0 +1,9 @@
+export type responseType = {
+    name: string,
+    carrier: string,
+    countryCode: string,
+    phoneNumber: string,
+    location: string,
+    fraud: null,
+    nationalFormat: string
+}
