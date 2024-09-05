000399536332d4b5c3699af1105022db519df608
waterlulu12 <aatishshrestha123@gmail.com>
2024-09-06 02:44:57 +0545

    chore: added fakeData for demo

---
+++ src/utils/reviewData.ts
@@ -0,0 +1,16 @@
+import { reviewType } from "./types/reviewTypes";
+
+export const reviewData: reviewType[] = [
+  {
+    name: "Elliott Lipinsky",
+    title: 'Law Offices Of Elliott Owen Lipinsky, Selma and Montgomery, Alabama',
+    review: "As a law firm owner, it is difficult to manage the volume of calls we receive daily. echowin provides fast and personal responses to each of our clients. This maintains a healthy attorney client relationship. At the same time, it increases our efficiency when dealing with important calls.",
+    rating: 5,
+  },
+  {
+    name: "Darren Elder",
+    title: 'Swift-Net.ca (ISP), Alberta & Saskatchewan',
+    review: "echowin is transforming how Swift-Net.ca engages with technology, making it more intuitive and accessible for our valued customers.",
+    rating: 4,
+  }
+];
