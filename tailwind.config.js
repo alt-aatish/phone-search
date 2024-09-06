24e784f80dcb57a4bf9a1ceaa7314fbd607af2b3
waterlulu12 <aatishshrestha123@gmail.com>
2024-09-06 18:37:39 +0545

    chore: added more animation keyframes

--- tailwind.config.js
+++ tailwind.config.js
@@ -30,11 +30,17 @@ module.exports = {
           "100%": {
             opacity: "0",
             height: "0",
+            zIndex: "-1",
           },
         },
         fadeInToView: {
           from: { opacity: "0" },
           to: { opacity: "1" },
+        },
+        fadeInReview: {
+          "0%": { opacity: "0" },
+          "40%": { opacity: "0.8" },
+          "100%": { opacity: "1" },
         },
         placeholderwave: {
           "0%": { transform: "translateX(-100%)" },
@@ -42,12 +48,14 @@ module.exports = {
           "100%": { transform: "translateX(100%)" },
         },
         slideInFromBottom: {
-          "0%": { transform: "translateX(100%)", opacity: "0" },
-          "100%": { transform: "translateX(0)", opacity: "1" },
+          "0%": { transform: "translateY(100%)", opacity: "0" },
+          "40%": { opacity: "0" },
+          "100%": { transform: "translateY(0)", opacity: "1" },
         },
         slideOutToTop: {
-          "0%": { transform: "translateX(0)", opacity: "1" },
-          "100%": { transform: "translateX(-100%)", opacity: "0" },
+          "0%": { transform: "translateY(0)", opacity: "1" },
+          "20%": { opacity: "0" },
+          "100%": { transform: "translateY(-100%)", opacity: "0" },
         },
       },
       animation: {
@@ -55,8 +63,9 @@ module.exports = {
         growThenFadeIn: "growThenFadeIn 0.8s ease-out forwards",
         placeholderwave: "placeholderwave 2s linear infinite",
         fadeInToView: "fadeInToView 1.5s ease-in-out 0s 1 normal both",
-        slideInFromBottom: "slideInFromBottom 1.2s ease-out forwards",
-        slideOutToTop: "slideOutToTop 1.2s ease-out forwards",
+        fadeInReview: "fadeInReview 1s ease-in-out 0s 1 normal both",
+        slideInFromBottom: "slideInFromBottom 1.5s ease-out forwards",
+        slideOutToTop: "slideOutToTop 1.5s ease-out forwards",
       },
     },
   },
